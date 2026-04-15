166a3ec26f433676adc91d085d60fdefb324b0fb
Rahul Tiwari <rahul.rkt007@gmail.com>
2026-04-16 01:36:12 +0530

    yaml file created

--- playwright.config.js
+++ playwright.config.js
@@ -32,6 +32,13 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
+    /* 🔥 REPORTERS – Jenkins friendly */
+  reporter: [
+    ['list'],
+    ['html', { open: 'never', outputFolder: 'playwright-report' }],
+    ['json', { outputFile: 'playwright-report/results.json' }],
+    ['allure-playwright', { outputFolder: 'allure-results' }],
+  ],
 
   /* Configure projects for major browsers */
   projects: [
